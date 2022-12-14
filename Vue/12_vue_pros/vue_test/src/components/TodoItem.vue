684d2f7212033614cd4dda094d28adaa7811cd59
UU <esinsis@163.com>
2022-12-14 23:24:30 +0800

    todo list demo

---
+++ Vue/12_vue_pros/vue_test/src/components/TodoItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <li>
+    <label>
+      <input type="checkbox" :checked="todoObj.done" @change="handleCheck(todoObj.id)"/>
+      <span>{{todoObj.content}}</span>
+    </label>
+    <button class="btn btn-danger" @click="handleDelete(todoObj.id)">删除</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ['todoObj', 'checkTodo', 'deleteItem'],
+  methods: {
+    handleCheck(id){
+      this.checkTodo(id)
+    },
+    handleDelete(id){
+      this.deleteItem(id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+li:hover {
+  background-color: #dddddd;
+}
+
+li:hover button {
+  display: block;
+}
+</style>
