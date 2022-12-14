02543b12609d64c1f937c0b4ba4a46a38f8afed1
UU <esinsis@163.com>
2022-12-14 23:27:03 +0800

    backup todo list demo

---
+++ Vue/12_vue_pros/vue_test/02_src_todoList/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <TodoHeader :addTodo="addTodo"/>
+        <TodoList :todos="todos" :checkTodo="checkTodo" :deleteItem="deleteItem"/>
+        <TodoFooter :todos="todos" :checkAllTodo="checkAllTodo" :clearAll="clearAll"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader';
+import TodoFooter from './components/TodoFooter';
+import TodoList from './components/TodoList';
+import TodoItem from './components/TodoItem';
+
+export default {
+  name: "App",
+  components: {
+    TodoHeader, TodoFooter, TodoList, TodoItem
+  },
+  data() {
+    return {
+      todos: [
+        {id: '001', content: '吃饭', done: true},
+        {id: '002', content: '睡觉', done: false},
+        {id: '003', content: '烫头', done: true},
+        {id: '004', content: '抽烟', done: false},
+      ]
+    }
+  },
+  methods: {
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj)
+    },
+    checkTodo(id) {
+      this.todos.forEach((i) => {
+        if (i.id === id) i.done = !i.done
+      })
+    },
+    deleteItem(id) {
+      this.todos = this.todos.filter(i => i.id !== id)
+    },
+    checkAllTodo(val) {
+      this.todos.forEach((i) => {
+        i.done = val
+      })
+    },
+    clearAll(){
+      this.todos = this.todos.filter(i => !i.done)
+    }
+  }
+}
+</script>
+
+<style>
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+</style>
+
---
+++ Vue/12_vue_pros/vue_test/02_src_todoList/components/TodoFooter.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="todo-footer">
+    <label>
+      <input type="checkbox" :checked="allChecked" @change="checkAll" id="footer"/>
+    </label>
+    <span>
+      <span>{{ doneTotal }}</span> / {{ todos.length }}
+      </span>
+    <button class="btn btn-danger" @click="clearAllChecked">清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoFooter",
+  props: ['todos', 'checkAllTodo', 'clearAll'],
+  computed: {
+    doneTotal() {
+      return this.todos.reduce((pre, todo) => pre + (todo.done ? 1 : 0), 0)
+    },
+    allChecked: {
+      get() {
+        return this.doneTotal === this.todos.length && this.todos.length > 0
+      },
+      // allChecked 被修改时调用
+      set(val) {
+        this.checkAllTodo(val)
+      }
+    }
+  },
+  methods: {
+    checkAll(e){
+      // 计算属性不能直接修改，只能通过修改用于计算该属性的原始变量来改变
+      // this.allChecked = !this.allChecked
+      this.checkAllTodo(e.target.checked)
+    },
+    clearAllChecked(){
+      this.clearAll()
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*footer*/
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
