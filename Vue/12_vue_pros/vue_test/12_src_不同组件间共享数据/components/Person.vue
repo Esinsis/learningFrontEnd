ddf70af30955d7f8b1a60098d9975d0bc750a5af
UU <esinsis@163.com>
2022-12-20 12:30:34 +0800

    组件间共享数据

---
+++ Vue/12_vue_pros/vue_test/12_src_不同组件间共享数据/components/Person.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>人员列表</h1>
+    <h3 style="color:red">Count组件求和为：{{sum}}</h3>
+    <input type="text" placeholder="请输入名字" v-model="name">
+    <button @click="add">添加</button>
+    <ul>
+      <li v-for="p in personList" :key="p.id">{{p.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {nanoid} from "nanoid";
+import {mapState} from "vuex";
+export default {
+  name: "Person",
+  data(){
+    return{
+      name: ''
+    }
+  },
+  methods: {
+    add(){
+      this.$store.commit('ADD_PERSON', {id: nanoid(), name: this.name})
+      this.name = ''
+    }
+  },
+  computed: {
+    // personList (){
+    //   return this.$store.state.personList
+    // },
+    ...mapState(['personList', 'sum'])
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
