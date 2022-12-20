2b2130e7324c6c79808e5681afbaaf12b2ba7e0f
UU <esinsis@163.com>
2022-12-20 12:08:13 +0800

    mapState 和 mapGetters

---
+++ Vue/12_vue_pros/vue_test/10_src_求和案例_mapState和mapGetters/store/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const actions = {
+    oddPlus(context, value) {
+        if (context.state.sum % 2) {
+            context.commit('INCREMENT', value)
+        }
+    },
+    delayPlus(context, value) {
+        setTimeout(() => {
+            context.commit('INCREMENT', value)
+        }, 500)
+    }
+}
+
+const mutations = {
+    INCREMENT(state, value) {
+        state.sum += value
+    },
+    DECREMENT(state, value) {
+        state.sum -= value
+    }
+}
+
+// vuex 中的getter 类似于Vue 中的计算属性
+const getters = {
+    multiTen(state) {
+        return state.sum * 10
+    }
+}
+
+const state = {
+    sum: 0
+}
+
+// 创建并暴露store
+export default new Vuex.Store({
+    actions,
+    mutations,
+    state,
+    getters
+})
